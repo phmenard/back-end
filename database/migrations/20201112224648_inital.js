3a5f8c90823518fb8692032363d484767ed116cf
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-11-12 23:36:50 -0500

    first migration

---
+++ database/migrations/20201112224648_inital.js
@@ -0,0 +1,45 @@
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('users', users => {
+            users.increments();
+            users.string('username', 255).notNullable().unique();
+            users.string('email', 255).notNullable().unique();
+            users.string('password', 255).notNullable();
+        })
+
+        .createTable('recipe', recipie => {
+            recipie.increments();
+            recipie.string('title', 255).notNullable().unique();
+            
+            recipie.integer('sourceId').unsigned().notNullable()
+                .references('id')
+                .inTable('users');
+            
+            recipie.text('instructions').notNullable();
+        })
+        
+        .createTable('category', category =>{
+            category.increments();
+            category.string('name').notNullable().unique();
+            category.integer('recipieId').unsigned().notNullable()
+                .references('id')
+                .inTable('recipe');
+        })
+
+        .createTable('ingredients', ingredients => {
+            ingredients.increments();
+            ingredients.string('description').notNullable();
+            ingredients.integer('recipieId').unsigned().notNullable()
+                .references('id')
+                .inTable('recipe');
+        });
+};
+
+
+
+exports.down = function (knex, Promise) {
+    return knex.schema
+         .dropTableIfExists('users')
+         .dropTableIfExists('recipie');
+};
+
