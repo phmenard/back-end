fdd2c57fdcf528aff0a822c73e9f7bc05f4da001
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-11-12 23:35:58 -0500

    update server

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const cookieParser = require("cookie-parser")
+const {restrict} = require('../auth/authenticate-middleware.js');
+const authRouter = require('../auth/auth-router.js');
+const recipeRouter = require('../recipies/recipe-router');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use(cookieParser())
+
+server.use('/api/auth', authRouter);
+server.use('/api/recipies',  restrict(), recipeRouter);
+
+module.exports = server;
