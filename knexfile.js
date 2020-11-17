0eff8e40ce778dbcfe9aa133cca4de69dfdee9ee
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-11-16 20:06:58 -0500

    latest

--- knexfile.js
+++ knexfile.js
@@ -19,4 +19,55 @@ module.exports = {
     },
     seeds: { directory: './database/seeds' },
   },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "../database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+    useNullAsDefault: true
+  },
 };
+
+//require('dotenv').config();
+
+/*module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'knex_migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  }, testing: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL, pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'knex_migrations',
+    },
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds/",
+    },
+    useNullAsDefault: true
+  },*/
